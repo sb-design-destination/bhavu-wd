8946fc9c2199a0d2697ea4d09a2f0e1abadfb2cd
Bhavyata Savaliya <109578378+sb-design-destination@users.noreply.github.com>
2022-09-05 10:13:20 +0530

    Add files via upload

---
+++ promt.js
@@ -0,0 +1,9 @@
+var promptresmarks = prompt("Enter marks");
+
+if (promptresmarks <= 50) {
+    console.log("promptresmarks");
+    document.write("pass class");
+} else {
+    console.log("try again");
+    document.write("try again");
+}
